129582d6b3e61cb01797a3de364742cd22b32862
harsh <harsh.vekariya@trootech.com>
2023-09-01 17:59:56 +0530

    commets reslove

--- tms/clickup/urls.py
+++ tms/clickup/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path, include
+from django.urls import path
 from clickup import views
 
 urlpatterns = [
     path("register/", views.SingUpView.as_view(), name="register"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("create_project/", views.ProjectCreateView.as_view(), name="Create_project"),
-    path("update_project/<int:pk>/",views.ProjectUpdateView.as_view(),name="update_project"),
-    path("delete_project/<int:pk>/",views.ProjectDeleteView.as_view(),name="delete_project"),
+    path("project/<int:pk>/", views.ProjectRetrieveUpdateDestroyView.as_view(), name="project"),
 ]
--- tms/clickup/views.py
+++ tms/clickup/views.py
@@ -1,11 +1,13 @@
 from rest_framework import status
-from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateAPIView, ListAPIView, DestroyAPIView
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
 from clickup.serializer import RegistrationSerializer, LoginSerializer, ProjectSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
 from django.http import JsonResponse
 from clickup.models import Project
+from rest_framework.viewsets import ModelViewSet
 
 
 class SingUpView(CreateAPIView):
@@ -26,7 +28,6 @@ class LoginView(ObtainAuthToken):
 class ProjectCreateView(ListCreateAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -36,10 +37,15 @@ class ProjectCreateView(ListCreateAPIView):
                              "data": serializer.validated_data}, status=status.HTTP_201_CREATED)
 
 
-class ProjectUpdateView(RetrieveUpdateAPIView):
+class ProjectRetrieveUpdateDestroyView(RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [IsAuthenticated]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return JsonResponse({"message": "Project deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
     def update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
@@ -48,15 +54,4 @@ class ProjectUpdateView(RetrieveUpdateAPIView):
         serializer.is_valid(raise_exception=True)
         self.perform_update(serializer)
         return JsonResponse({"message": "Project updated successfully",
-                             "data": serializer.validated_data}, status=status.HTTP_200_OK)
-
-
-class ProjectDeleteView(DestroyAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [IsAuthenticated]
-
-    def destroy(self, request, *args, **kwargs):
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return JsonResponse({'message': 'Project deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+                             "data": serializer.data}, status=status.HTTP_200_OK)
