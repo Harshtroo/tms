b8ac6d78f29e2e60ae65af37252b1069460e3c77
harsh <harsh.vekariya@trootech.com>
2023-09-04 18:27:56 +0530

    comments resolve

--- tms/tms/urls.py
+++ tms/tms/urls.py
@@ -1,8 +1,5 @@
 from django.urls import path, include
-from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-from django.conf import settings
-from django.views.generic.base import TemplateView
 
 urlpatterns = [
 
